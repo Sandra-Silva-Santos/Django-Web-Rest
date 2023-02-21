ff688d5b1def48975e86e7e05d38dcb65ff93515
Sandra <sandra_silva_santos@live.com>
2023-02-20 23:28:34 -0300

    teste descrição labs

--- manage.py
+++ manage.py
@@ -6,15 +6,22 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    # Set the environment variable for the Django settings module.
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "progeto.settings")
     try:
+        # Import the execute_from_command_line() function from the
+        # django.core.management module.
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        # If the import fails, raise an ImportError exception and
+        # include a custom message.
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    # Call execute_from_command_line() to execute the command-line
+    # utility.
     execute_from_command_line(sys.argv)
 
 
